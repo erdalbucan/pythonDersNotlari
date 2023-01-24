b925ae122a5ffd4db6b3ff135bbcd43ad5c3bd06
Erdal BUCAN <erdalbucan@Erdal-MacBook-Pro.local>
2023-01-24 03:41:39 +0200

    Dosyalarin GITHUB Yuklemesi

---
+++ 01_Giris/0_2_Kurulum_ve_Baslangic/sayfa_ekleme.py
@@ -0,0 +1,5 @@
+#sayfa ekyelip github a update etmek icin 
+print("visual basic")
+print(int(input("ikiye carpmak istediginiz sayiyi giriniz :"))*2)
+print("girdigininz sayinin ikiye carpimi ekrandadir")
+print ("deneme sayfasi idi")
